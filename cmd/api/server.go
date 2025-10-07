1c485aca35ff76704c00514a2ea3ad8bfc93e158
ikrom <ikhakimov3@gmail.com>
2025-10-07 16:07:00 +0500

    Lesson 7.25

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type user struct {
@@ -14,106 +13,33 @@ type user struct {
 	City string `json:"city"`
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func teacherHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Println(r.URL.Path)
+		path := strings.TrimPrefix(r.URL.Path, "/teachers/")
+		userID := strings.TrimSuffix(path, "/")
+
+		fmt.Println(userID)
+	}
+}
+
+func studnetsHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	port := ":3000"
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Root Route")
-		w.Write([]byte("Hello Root Route"))
-		fmt.Println("Hello Root Route")
-	})
+	http.HandleFunc("/", rootHandler)
 
-	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello Teachers Route")
-		fmt.Println(r.Method)
-		switch r.Method {
-		case http.MethodGet:
-			w.Write([]byte("Hello GET Method"))
-		case http.MethodPost:
-			// Parse form data
-			err := r.ParseForm()
-			if err != nil {
-				http.Error(w, "Error parsing form", http.StatusBadRequest)
-				return
-			}
-			w.Write([]byte("Hello POST Method"))
-			fmt.Println("Form:", r.Form)
+	http.HandleFunc("/teachers/", teacherHandler)
 
-			// Prepare response data
-			response := make(map[string]interface{})
-			for key, value := range r.Form {
-				response[key] = value[0]
-			}
-
-			// RAW Body
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				return
-			}
-			defer r.Body.Close()
-
-			fmt.Println("RAW Body:", body)
-			fmt.Println("RAW Body:", string(body))
-			fmt.Println("Response Map", response)
-
-			// If you expect json data, then unmarshal it
-			var userInstance user
-			err = json.Unmarshal(body, &userInstance)
-			if err != nil {
-				fmt.Println("Error:", err)
-				return
-			}
-			fmt.Println(userInstance.Name)
-			fmt.Println("Response Map End")
-
-			response1 := make(map[string]interface{})
-			for key, value := range r.Form {
-				response[key] = value[0]
-			}
-
-			err = json.Unmarshal(body, &response1)
-			if err != nil {
-				fmt.Println("Error:", err)
-				return
-			}
-			fmt.Println("Unmarshalled JSON into a map", response1)
-
-			fmt.Println("Body:", r.Body)
-			fmt.Println("Body:", r.Form)
-			fmt.Println("Header:", r.Header)
-			fmt.Println("Context:", r.Context())
-			fmt.Println("ContextLength:", r.ContentLength)
-			fmt.Println("Host:", r.Host)
-			fmt.Println("Method:", r.Method)
-			fmt.Println("Protocol:", r.Proto)
-			fmt.Println("Remote Addr:", r.RemoteAddr)
-			fmt.Println("Request URI:", r.RequestURI)
-			fmt.Println("TLS:", r.TLS)
-			fmt.Println("Trailer:", r.Trailer)
-			fmt.Println("Transfer Encoding:", r.TransferEncoding)
-			fmt.Println("URL:", r.URL)
-			fmt.Println("User Agent:", r.UserAgent())
-			fmt.Println("Port:", r.URL.Port())
-			fmt.Println("Scheme:", r.URL.Scheme)
-
-		case http.MethodPut:
-			w.Write([]byte("Hello PUT Method"))
-		case http.MethodPatch:
-			w.Write([]byte("Hello PATCH Method"))
-		case http.MethodDelete:
-			w.Write([]byte("Hello DELETE Method"))
-		}
-	})
-
-	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Students Route"))
-		fmt.Println("Hello Students Route")
-	})
-
-	http.HandleFunc("/execs", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Execs Route"))
-		fmt.Println("Hello Execs Route")
-	})
+	http.HandleFunc("/students", studnetsHandler)
 
 	fmt.Println("Server is running on port: ", port)
 	err := http.ListenAndServe(port, nil)
