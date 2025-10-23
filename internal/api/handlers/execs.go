a8bcf9c1b6f3d22af1dc38be5abf3a263260465a
ikrom <ikhakimov3@gmail.com>
2025-10-23 12:50:00 +0500

    Lesson 7.92

--- internal/api/handlers/execs.go
+++ internal/api/handlers/execs.go
@@ -465,3 +465,30 @@ func ForgotPasswordHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "Password reset link sent to %s", req.Email)
 }
+func ResetPasswordHandler(w http.ResponseWriter, r *http.Request) {
+	token := r.PathValue("resetcode")
+	type request struct {
+		NewPassword     string `json:"new_password"`
+		ConfirmPassword string `json:"confirm_password"`
+	}
+	var req request
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid values in request", http.StatusBadRequest)
+		return
+	}
+
+	if req.NewPassword != req.ConfirmPassword {
+		http.Error(w, "Passwords should match", http.StatusBadRequest)
+		return
+	}
+
+	err = sqlconnect.ResetPasswordDbHandler(token, req.NewPassword)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(w, "Password reset successfully")
+}
